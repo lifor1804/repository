8adde9f8cefce32a99750af300d9b484cbfee390
kypas7952 <kypas7952@8R4YP52.Student.UGDSB.ED>
2023-12-04 10:12:23 -0500

    Changed Header

---
+++ PFRProjectManagement/src/pfrprojectmanagement/PFRProjectManagement.java
@@ -0,0 +1,19 @@
+/*
+Kyle Pasta, Micah Ridler, Liam Fortin
+ */
+package pfrprojectmanagement;
+
+/**
+ *
+ * @author kypas7952
+ */
+public class PFRProjectManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
