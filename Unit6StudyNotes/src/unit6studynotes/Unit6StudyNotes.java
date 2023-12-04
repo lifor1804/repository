d92ecbc698c1fd412f521dfafd588aff638f1c89
mirid1871 <mirid1871@4J44C62.Student.UGDSB.ED>
2023-12-04 09:42:18 -0500

    added print statement

---
+++ Unit6StudyNotes/src/unit6studynotes/Unit6StudyNotes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unit6studynotes;
+
+/**
+ *
+ * @author mirid1871
+ */
+public class Unit6StudyNotes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Kyle a meany");
+    }
+    
+}
